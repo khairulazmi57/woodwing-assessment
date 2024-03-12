fe6c6a0c9a1021dfcfa8cec6d41f02fb0d1f6c79
Khairul Azmi <khairul.azmi57@gmail.com>
2024-03-12 09:32:36 +0800

    Initial commit

---
+++ src/main/java/com/example/distanceservice/DistanceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.distanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceserviceApplication.class, args);
+	}
+
+}
